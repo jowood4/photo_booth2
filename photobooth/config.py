8f12a82898a4f883b7255d6251254ce81542e213
Joe <jowood4@gmail.com>
2015-06-06 07:35:11 -0400

    initial commit with files

---
+++ photobooth/config.py
@@ -0,0 +1,13 @@
+# Copy this file to config.py and replace the values with your information
+# OAuth keys can be generated from https://api.tumblr.com/console/calls/user/info
+tumblr_consumer_key='I0Cm11SuNddMuFOVUey2yTiV2WQjq1dyAMZJlM4CB7qHqBWtMC' #replace with your key
+tumblr_consumer_secret='uujw8hWjypMUiI3WocTn3jqfogeREBT5DLYfhidtmNDaDGBORQ' #replace with your secret code
+tumblr_oath_token='7IqxeEnrsTe5kwJera1O56uWCNcB6qGIXfowFDUqHP2auZR5EQ' #replace with you oath token
+tumblr_oath_secret='xJ3GU3pSfhzYF4NzANkm9XTvOoVOIm0rbT3uQ7QjMI28X62CDL' #replace with your oath secret code
+tumblr_blog = 'jumpedtheshark' # replace with your tumblr account name without .tumblr.com
+twitter_CONSUMER_KEY = 'c1i6u53Co8NWOVjkda4yoipt0'
+twitter_CONSUMER_SECRET = '9VqN5zmgQ4wgD7YKtQvIbZv6WhIDFyTGdo4nGGbzgn5JN3W5Lq'
+twitter_ACCESS_KEY = '3234263421-gSqQw012LFOCMwRQSi2lBmOwnyAZEZVJ0TZSKh8'
+twitter_ACCESS_SECRET = '7K56EIHk2wWZV0wpNBRG90CEtMI63d3q5N8A7bjxZDeUA'
+file_path = '/home/pi/photobooth/pics/' # path to save images
+backup_path = '/home/pi/photobooth/backups/' # path to backup images
